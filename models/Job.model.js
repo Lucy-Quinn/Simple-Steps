add50d5d6c33bf7176b9ca1036e133462dbf9013
Lucy Quinn <lucy.quinn.uk@gmail.com>
2020-11-19 14:57:44 +0100

    add user and job models

---
+++ models/Job.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const jobSchema = new Schema(
+    {
+        title: { type: String, require: true },
+        date: { type: Date, require: true },
+        address: {
+            street: { type: String, default: '' }
+        },
+        location: {
+            type: {
+                type: String
+            },
+            coordinates: [Number]
+        },
+        charity: { type: Schema.Types.ObjectId, ref: "User" },
+        volunteers: [{
+            volunteer: { type: Schema.Types.ObjectId, ref: "User" },
+            accepted: Boolean
+        }
+        ]
+    },
+    {
+        timestamps: {
+            createdAt: 'create_at',
+            updatedAt: 'updated_at'
+        }
+    }
+)
+
+const Job = mongoose.model('Job', jobSchema);
+module.exports = Job;
--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,35 @@
 // Model
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: String,
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    description: { type: String, maxlength: 280 },
+    photo: { type: String, default: 'https://www.dovercourt.org/wp-content/uploads/2019/11/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.jpg' },
+    location: {
+        type: {
+            type: String
+        },
+        coordinates: [Number]
+    },
+    age: Number, //only for volunteer
+    skills: [String], //only for volunteer
+    jobsApplied: [{ type: Schema.Types.ObjectId, ref: "Job" }], //only for volunteer
+    jobsCreated: [{ type: Schema.Types.ObjectId, ref: "Job" }], //only for charity
+    userType: {
+        type: String, enum: ['charity', 'volunteer'], required: true
+    }
+},
+    {
+        timestamps: {
+            createdAt: 'create_at',
+            updatedAt: 'updated_at'
+        }
+    }
+)
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
